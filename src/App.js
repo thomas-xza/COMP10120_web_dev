e2ec479ff8be328b84a62fe0ccac4441023b1c74
m-wizard <152719226+m-wizard@users.noreply.github.com>
2023-12-02 22:12:05 +0000

    OK awesome, by hashing 2 separate Mapbox guides together (though there are probably API docs somewhere...), figured out how to capture co-ordinates on mouseclick.

--- src/App.js
+++ src/App.js
@@ -7,11 +7,17 @@ mapboxgl.accessToken = 'pk.eyJ1Ijoid2l6YXJkLXQiLCJhIjoiY2xwaDk1cGg5MDU5MzJtczV6O
 export default function App() {
     const mapContainer = useRef(null);
     const map = useRef(null);
-    const [lng, setLng] = useState(-70.9);
-    const [lat, setLat] = useState(42.35);
-    const [zoom, setZoom] = useState(9);
+    const [lng, setLng] = useState(-2.235);
+    const [lat, setLat] = useState(53.46235);
+    const [zoom, setZoom] = useState(13);
 
     //  https://docs.mapbox.com/api/maps/styles/
+
+    function map_clicked(e) {
+
+	alert("You clicked:\n" + lng + "\n" + lat);
+
+    }
     
     useEffect(() => {
 	if (map.current) return; // initialize map only once
@@ -23,29 +29,27 @@ export default function App() {
 	    zoom: zoom
 	});
 	
-	// map.current.on('move', () => {
-	//     setLng(map.current.getCenter().lng.toFixed(4));
-	//     setLat(map.current.getCenter().lat.toFixed(4));
-	//     setZoom(map.current.getZoom().toFixed(2));
-	// });
 	map.current.on('mousemove', (e) => {
 
-	    console.log(JSON.stringify(e));
-	    // setLng(map.current.getCenter().lng.toFixed(4));
-	    // setLat(map.current.getCenter().lat.toFixed(4));
-	    // setZoom(map.current.getZoom().toFixed(2));
+	    console.log(JSON.stringify(e.point));
+	    const co_ords = e.lngLat.wrap();
+
+	    console.log(co_ords, co_ords["lng"], co_ords["lat"]);
+	    setLng(co_ords.lng);
+	    setLat(co_ords.lat);
+	    
+	    setZoom(map.current.getZoom().toFixed(2))
+	    
 	});
 
-	console.log(lng, lat);
     });
     
     return (
 	    <div>
-	    <button className="proof-of-concept button" onClick={() => console.log(lng, lat)}>Log longitude/latitude to console</button>
 	    <div className="sidebar">
 	    Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
 	</div>
-	    <div ref={mapContainer} className="map-container" />
+	    <div ref={mapContainer} className="map-container" onClick={map_clicked}/>
 	    </div>
     );
 }
